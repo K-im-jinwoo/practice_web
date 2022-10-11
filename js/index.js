ec9e01ec77a8f35e671b0b0fec4fc6499f555422
zaqaz113 <zaqaz113@naver.com>
2022-10-11 21:22:06 +0900

    탭메뉴 스크립트

--- js/index.js
+++ js/index.js
@@ -21,3 +21,15 @@ $(".ban").slick({
   autoplaySpeed: 3000,
   dots: true
 });
+
+// 탭메뉴
+var $tab_list = $(".tab_menu");
+$tab_list.find("ul ul").hide();
+$tab_list.find("li.active > ul").show();
+
+function tabMenu(e){
+  e.preventDefault();
+  var $this = $(this);
+  $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
+}
+$tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
