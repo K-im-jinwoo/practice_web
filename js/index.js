bfb43aeb0f081ae97bca768985df51259914cde4
zaqaz113 <zaqaz113@naver.com>
2022-10-11 21:41:36 +0900

    갤러리 스크립트

--- js/index.js
+++ js/index.js
@@ -33,3 +33,27 @@ function tabMenu(e){
   $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
 }
 $tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
+
+// 갤러리
+$(".gallery_img").slick({
+  fade: true,
+  pauseOnHover: true,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 300,
+  slidesToShow: 1
+});
+$(".play").click(function(){
+  $(".gallery_img").slick("slickPlay")
+})
+$(".pause").click(function(){
+  $(".gallery_img").slick("slickPause")
+})
+$(".prev").click(function(){
+  $(".gallery_img").slick("slickPrev")
+})
+$(".next").click(function(){
+  $(".gallery_img").slick("slickNext")
+})
